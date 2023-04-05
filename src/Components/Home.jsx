45b2ac277306157138d6b41156cb0c0617f4402a
Jaro1407 <jaime.sft@gmail.com>
2023-04-05 15:03:14 -0600

    Added menu

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Menu from "./Menu"
 
 function Home() {
 
     return(
         <div>
             <h1 className="display-5">Home</h1>
+            <Menu/>
         </div>
     )
 }
